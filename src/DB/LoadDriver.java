dd705a9709018df176881c931ec8b05ba2a0db7a
José Olinda <joseolindad@yahoo.com.br>
2019-12-03 15:43:48 -0300

    Resolução do Exercício de Revisão

--- src/DB/LoadDriver.java
+++ src/DB/LoadDriver.java
@@ -4,14 +4,18 @@ public class LoadDriver {
 
     public static boolean load() {
         try {
+            
             Class.forName("com.mysql.cj.jdbc.Driver")
                     .newInstance();
             System.out.println("JDBC carregado");
             return true;
+            
         } catch (Exception erro) {
+            
             System.err.println("JDBC não encontrado");
             System.err.println(erro);
             return false;
+            
         }
     }
 }
